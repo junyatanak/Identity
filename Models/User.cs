26b53200dd7f00755cba12ae09a1486f2033159b
junyatanak <piletree0418@gmail.com>
2026-04-17 20:35:18 +0700

    Added properties to User.cs and Create.cshtml

--- Models/User.cs
+++ Models/User.cs
@@ -11,4 +11,9 @@ namespace Identity.Models;
     public required string Email { get; set; }
     [Required]
     public required string Password { get; set; }
+    public Country Country { get; set; }
+    public int Age { get; set; }
+    [Required]
+    public string Salary { get; set; }=string.Empty;
+    
     }
