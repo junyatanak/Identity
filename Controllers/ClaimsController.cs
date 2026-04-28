64bdb3c5668f1630830de593e3c7b9ec33294250
junyatanak <piletree0418@gmail.com>
2026-04-28 07:10:37 +0700

    added http create and action method in claimcontroller

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -23,8 +23,49 @@ namespace Identity.Controllers
         {
             var user = await userManager.GetUserAsync(User);
             var claim = new Claim(claimType,claimValue,ClaimValueTypes.String);
+            var result = await userManager.AddClaimAsync(user,claim);
+
+            if (!result.Succeeded)
+            {
+                Errors(result);
+                return View();
+            }
+                
+            return RedirectToAction("Index");
         
         }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string claimValues)
+        {
+            var user = await userManager.GetUserAsync(User);
+            var claimValuesArray = claimValues.Split(";");
+            var claimType = claimValuesArray[0];
+            var claimValue = claimValuesArray[1];
+            var claimIssuer = claimValuesArray[2];
+
+            var claim = User.Claims.Where(x => x.Type == claimType && x.Value == claimValue && x.Issuer == claimIssuer).FirstOrDefault();
+            var result = await userManager.RemoveClaimAsync(user,claim);
+           
+            if (!result.Succeeded)
+            {
+                Errors(result);
+                return View("Index");
+            }
+                
+            return RedirectToAction("Index");
+
+        }
+
+
+
+        private void Errors(IdentityResult result)
+        {
+            foreach(IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("",error.Description);
+            }
+        }
+
 
     }
 }
